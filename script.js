ff6a9377c88b18293534656d6a6ca5994ca9ebbb
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2025-03-11 09:11:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+        let name = document.getElementById('name');
+        let year = document.getElementById('year');
+        let url = document.getElementById('url');
+        let button = document.getElementById('button');
+
+        button.addEventListener('click',()=>{
+            url.innerText = "https://localhost:8080/";
+            let nameValue = name.value;
+            let yearValue = year.value;
+            
+            if(nameValue.length != 0 && yearValue.length !=0){
+                url.innerText += `?name=${nameValue}&year=${yearValue}`;
+            }
+            else if(nameValue.length != 0 && yearValue.length ==0){
+                url.innerText += `?name=${nameValue}`;
+            }
+            else{
+                url.innerText += `?year=${yearValue}`;
+            }
+        });
